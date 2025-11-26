61e3ce608f46f16e53bd04685db50bb222f15da3
dobin <dobinshin@gmail.com>
2025-11-26 17:12:17 +0900

    c: add prettier and fix

--- app/upbit/page.tsx
+++ app/upbit/page.tsx
@@ -1,8 +1,8 @@
-export default function Page () {
+export default function Page() {
     return (
         <div>
-        <h1>Upbit Page</h1>
-        <p>Welcome to the Upbit page!</p>
+            <h1>Upbit Page</h1>
+            <p>Welcome to the Upbit page!</p>
         </div>
-    )
+    );
 }
