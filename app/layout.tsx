e972aad14dfd44c67bf1ea25bcb7fa7885f05aca
dobin <dobinshin@gmail.com>
2025-12-08 12:34:07 +0900

    u: html lang to ko

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="ko">
             <body
                 className={`bithumb-font ${geistSans.variable} ${geistMono.variable} antialiased`}
             >
