92f88fa4be2c43332a77bd88e034a8e9187a4c52
dobin <dobinshin@gmail.com>
2025-12-16 19:51:22 +0900

    c: 검색창에 트랜드 리스트 추가

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -48,10 +48,11 @@ function SheetContent({
     className,
     children,
     side = "right",
+    xIcon = true,
     ...props
 }: React.ComponentProps<typeof SheetPrimitive.Content> & {
     side?: "top" | "right" | "bottom" | "left";
-}) {
+} & { xIcon?: boolean }) {
     return (
         <SheetPortal>
             <SheetOverlay />
@@ -72,10 +73,12 @@ function SheetContent({
                 {...props}
             >
                 {children}
-                <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
-                    <XIcon className="size-4" />
-                    <span className="sr-only">Close</span>
-                </SheetPrimitive.Close>
+                {xIcon && (
+                    <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
+                        <XIcon className="size-4" />
+                        <span className="sr-only">Close</span>
+                    </SheetPrimitive.Close>
+                )}
             </SheetPrimitive.Content>
         </SheetPortal>
     );
