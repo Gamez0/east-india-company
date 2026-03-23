f10409b31068fd3a9596d45d6d416562ae66a72e
dobin <dobinshin@gmail.com>
2026-03-23 13:09:29 +0900

    c: wrap Suspense where using browser api

--- app/bunjang/test4/page.tsx
+++ app/bunjang/test4/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Filters from "./Filters";
 
 export default function Page() {
@@ -5,7 +6,9 @@ export default function Page() {
         <main>
             문제에 따라 다르지.. useState를 써야 하면 이제 거긴 클라이언트
             컴포넌트로 하고
-            <Filters />
+            <Suspense fallback={<div>Loading ...</div>}>
+                <Filters />
+            </Suspense>
         </main>
     );
 }
