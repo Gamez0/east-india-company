2a2b63b4e8bd45db709c8c9484e14b5eaafa9e6f
dobin <dobinshin@gmail.com>
2026-04-11 23:27:14 +0900

    refactor(root, bithumb): cn import 경로 수정

--- apps/bithumb/components/ui/pagination.tsx
+++ apps/bithumb/components/ui/pagination.tsx
@@ -5,7 +5,7 @@ import {
     MoreHorizontalIcon,
 } from "lucide-react";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@repo/lib/utils";
 import { buttonVariants, type Button } from "@repo/ui/button";
 
 function Pagination({ className, ...props }: React.ComponentProps<"nav">) {
