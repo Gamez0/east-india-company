6e78f006ffc0575e13da085cbaf2fd7fc939254b
dobin <dobinshin@gmail.com>
2026-04-11 23:04:14 +0900

    d: 이전 라우트 기반 레포 제거

--- app/bunjang/test2/SearchInput.tsx
+++
@@ -1,9 +0,0 @@
-export default function SearchInput() {
-    return (
-        <form action="" method="GET">
-            <input name="q" />
-            <input type="hidden" name="page" value={1} />
-            <button type="submit">검색</button>
-        </form>
-    );
-}
